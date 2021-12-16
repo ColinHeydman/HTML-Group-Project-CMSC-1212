d09bed29d96f64f903595646ecba4dcf4cba706b
Chevy Rios <mv8865pr@go.minnstate.edu>
2021-12-16 00:15:01 -0600

    Added events page, touched up home page

---
+++ office_events.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/*
+   
+   Filename:   office_events.js   
+
+*/
+
+var dayEvent = new Array();
+
+dayEvent[1] = "";
+dayEvent[2] = "<br />Binge Time:<br />Season 1<br />3 pm";
+dayEvent[3] = "<br />Binge Time:<br />Season 2 pt 1<br />1 pm";
+dayEvent[4] = "<br />Binge Time:<br />Season 2 pt 2<br />1 pm";
+dayEvent[5] = "<br />Cosplay Contest<br />3 pm";
+dayEvent[6] = "<br />Dunder Mifflin<br />Bake-off<br />11am";
+dayEvent[7] = "";
+
+
+dayEvent[8] = "<br />The Office forums:<br />Dunderpedia<br />2 pm";
+dayEvent[9] = "<br />Binge Time:<br />Season 3 pt 1<br />1 pm";
+dayEvent[10] = "<br />Binge Time:<br />Season 3 pt 2<br />1 pm";
+dayEvent[11] = "<br />Binge Time:<br />Season 4 pt 1<br />1 pm";
+dayEvent[12] = "<br />Binge Time:<br />Season 4 pt 2<br />1 pm";
+dayEvent[13] = "";
+dayEvent[14] = "<br />Bookclub:<br />Bring fav fanfic<br />7 pm";
+
+dayEvent[15] = "<br />Binge Time:<br />Season 5 pt 1<br />1 pm";
+dayEvent[16] = "<br />Binge Time:<br />Season 5 pt 2<br />1 pm";
+dayEvent[17] = "<br />Binge Time:<br />Season 5 pt 3<br />1 pm";
+dayEvent[18] = "<br />Binge Time:<br />Season 6 pt 1<br />1 pm";
+dayEvent[19] = "<br />Binge Time:<br />Season 6 pt 2<br />1 pm";
+dayEvent[20] = "<br />Cosplay with Trivia<br />7 pm";
+dayEvent[21] = "";
+
+dayEvent[22] = "<br />Binge Time:<br />Season 7 pt 1<br />1 pm";
+dayEvent[23] = "<br />Binge Time:<br />Season 7 pt 2<br />1 pm";
+dayEvent[24] = "<br />Binge Time:<br />Season 8 pt 1<br />1 pm";
+dayEvent[25] = "<br />Binge Time:<br />Season 8 pt 1<br />1 pm";
+dayEvent[26] = "<br />Bookclub:<br />Bring fav fanfic<br />7 pm";
+dayEvent[27] = "";
+dayEvent[28] = "<br />The Office forums:<br />Dunderpedia<br />2 pm";
+
+dayEvent[29] = "<br />Binge Time:<br />Season 9 pt 1<br />1 pm";
+dayEvent[30] = "<br />Binge Time:<br />Season 9 pt 2<br />1 pm";
+dayEvent[31] = "";
