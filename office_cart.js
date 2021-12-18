e988cbb52eb29df5511c22aa34bddf4cc2f46096
Chevy Rios <mv8865pr@go.minnstate.edu>
2021-12-18 03:23:41 -0600

    Fixed order forms and added merch page

--- office_cart.js
+++ office_cart.js
@@ -5,52 +5,91 @@
 
    Filename: office_cart.js
    
-*/ 
+*/
 window.addEventListener("load", function () {
-    calcCart();
-    cart.elements.modelQty.onchange = calcCart;
+    var orderForm = document.forms.orderForm;
+    orderForm.elements.orderDate.value = new Date().toDateString();
 
-    var shippingOptions = document.querySelectorAll('input[name="shipping"]');
-    for (var i = 0; i < shippingOptions.length; i++) {
-        shippingOptions[i].onclick = calcCart;
+    orderForm.elements.model.focus();
+
+    calcOrder();
+
+    //Event handler for the web form
+    orderForm.elements.model.onchange = calcOrder;
+    orderForm.elements.qty.onchange = calcOrder;
+
+    var shipOptions = document.querySelectorAll('input[name="shipping"]');
+
+    var shipOptions = document.querySelectorAll('input[name="shipping"]');
+    for (var i = 0; i < shipOptions.length; i++) {
+        shipOptions[i].onclick = calcOrder;
     }
 });
 
-function calcCart() {
-    var orderForm = document.forms.cart;
+function calcOrder() {
+    var orderForm = document.forms.orderForm;
 
     //Calculate the initial cost of the order
-    var cost = orderForm.elements.modelCost.value;
-    var quantity = orderForm.elements.modelQty.value;
+    var mIndex = orderForm.elements.model.selectedIndex;
+    //alert(mIndex);
+    var mCost = orderForm.elements.model.options[mIndex].value;
+    //alert(mCost);
+    var qIndex = orderForm.elements.qty.selectedIndex;
+    //alert(qIndex);
+    var quantity = orderForm.elements.qty[qIndex].value;
+    //alert(quantity);
+    var initialCost = mCost * quantity;
 
-    var orderCost = cost * quantity;
-
-    orderForm.elements.orderCost.value = formatUSCurrency(orderCost);
+    orderForm.elements.initialCost.value = formatUSCurrency(initialCost);
 
     //Retrieve the cost of the protection plan
-    var shipCost = document.querySelector('input[name="shipping"]:checked').value * quantity;
+    var shipCost = document.querySelector('input[name="shipping"]:checked').value;
     orderForm.elements.shippingCost.value = formatNumber(shipCost, 2);
 
     //Calculate the order sub total
-    orderForm.elements.subTotal.value = formatNumber(orderCost * 1 + shipCost * 1, 2);
+    orderForm.elements.subtotal.value = formatNumber(initialCost * 1 + shipCost * 1, 2);
 
     //Calculate sales tax
-    var salesTax = 0.05 * (orderCost * 1 + shipCost * 1);
-    orderForm.elements.salesTax.value = formatNumber(salesTax, 2);
+    var salesTax = 0.05 * (initialCost * 1 + shipCost * 1);
+    orderForm.elements.salesTax.value = formatNumber(salesTax);
 
     //Calculate the order total
-    var totalCost = orderCost * 1 + shipCost * 1 + salesTax * 1;
-    orderForm.elements.cartTotal.value = formatUSCurrency(totalCost);
+    var totalCost = initialCost * 1 + shipCost * 1 + salesTax * 1;
+    orderForm.elements.totalCost.value = formatUSCurrency(totalCost);
+
+    orderForm.elements.modelName.value = orderForm.elements.model.options[mIndex].text;
 
     orderForm.elements.shippingType.value =
         document.querySelector('input[name="shipping"]:checked').nextSibling.nodeValue;
+
+    var image = document.getElementById('merchImg');
+    if (mIndex == 0) {
+        image.src = "img0.png";
+    }
+    else if (mIndex == 1) {
+        image.src = "img1.png";
+    }
+    else if (mIndex == 2) {
+        image.src = "img2.png";
+    }
+}
+
+function changeImage(img) {
+    img.src = img.src.replace("_t", "_b");
 }
 
 function formatNumber(val, decimals) {
-   return val.toLocaleString(undefined, {minimumFractionDigits: decimals, 
-                                         maximumFractionDigits: decimals});
+    return val.toLocaleString(undefined,
+        {
+            minimumFractionDigits: decimals,
+            maximumFractionDigits: decimals
+        });
 }
 
 function formatUSCurrency(val) {
-   return val.toLocaleString('en-US', {style: "currency", currency: "USD"} );
+    return val.toLocaleString('en-US',
+        {
+            style: "currency",
+            currency: "USD"
+        });
 }
